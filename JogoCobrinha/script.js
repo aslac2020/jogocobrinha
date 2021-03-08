7e681805a6877b216717c66f5e2a174792731fa8
aslac2020 <andredesenvolvedorti@gmail.com>
2021-03-07 22:06:58 -0300

    fim jogo

--- JogoCobrinha/script.js
+++ JogoCobrinha/script.js
@@ -15,13 +15,13 @@ let food = {
 }
 
 function criarBG(){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "green";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
     for ( i=0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "white";
         context.fillRect(snake[i].x, snake[i].y, box, box)      
     }
 }
@@ -48,8 +48,14 @@ function iniciarJogo(){
     if (snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
-    
 
+    for (i=1; i < snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :( ')
+        }
+    }
+    
     criarBG();
     criarCobrinha();
     drawFood();
